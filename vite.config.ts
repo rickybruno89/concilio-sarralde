d882b6c28cf11774a7dbc08f13e5a74cc8e90fa7
Ricky Bruno <19984174+rickybruno89@users.noreply.github.com>
2026-07-17 09:42:04 -0300

    feat: add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/concilio-sarralde/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
